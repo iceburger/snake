c3827a0513b95c6146605728266bc2b07132c1b9
iceburger <iceburger@yandex.ru>
2015-10-04 13:33:58 +0300

    lesson 4

--- snake/snake/Program.cs
+++ snake/snake/Program.cs
@@ -10,23 +10,9 @@ namespace snake
     {
         static void Main(string[] args)
         {
-            /*int x1 = 12;
-            int y1 = 14;
-            char sym1 = '*';*/
+            Point p1 = new Point(13, 7, '*');
 
-            /*Draw( x1, y1, sym1 );*/
-
-            Point p1 = new Point();
-
-            p1.x = 13;
-            p1.y = 7;
-            p1.sym = '*';
-
-            Point p2 = new Point();
-
-            p2.x = 7;
-            p2.y = 3;
-            p2.sym = '*';
+            Point p2 = new Point(7, 3, '#');
 
             p1.Draw();
             p2.Draw();
