e17ff7d9c1e3d0d3cd2d727b173f5d437524b0f0
iceburger <iceburger@yandex.ru>
2015-10-09 07:47:25 +0300

    les 7

--- snake/snake/Program.cs
+++ snake/snake/Program.cs
@@ -10,32 +10,13 @@ namespace snake
     {
         static void Main(string[] args)
         {
-            Point p1 = new Point(13, 7, '*');
+            hLine hline = new hLine(7, 12, 9, '*');
 
-            Point p2 = new Point(7, 3, '#');
+            hline.Drow();
 
-            p1.Draw();
-            p2.Draw();
+            vLine vline = new vLine(3, 7, 4, '#');
 
-            List<int> intList = new List<int>();
-
-            intList.Add( 0 );
-            intList.Add( 1 );
-            intList.Add( 2 );
-
-            int x = intList[0];
-            int y = intList[1];
-            int z = intList[2];
-
-            foreach (int i in intList)
-            {
-                Console.WriteLine( i );
-            }
-
-            List<Point> pList = new List<Point>();
-
-            pList.Add( p1 );
-            pList.Add( p2 );
+            vline.Drow();
 
             Console.ReadLine();
         }
