ddccb3d5325d16b620a60c8c022d51638e8c18d3
iceburger <iceburger@yandex.ru>
2015-10-09 07:50:26 +0300

    les 7 (reverted from commit a3237ed530340f824b61b72b0f17d813a2d86945) (reverted from commit 189cc6091495a165acf0ed260635dea2f29d16dd)

---
+++ snake/snake/vLine.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace snake
+{
+    class vLine : Figure
+    {
+
+        public vLine(int yTop, int yBottom, int x, char sym)
+        {
+            pList = new List<Point>();
+
+            for (int i = yTop; i <= yBottom; i++)
+            {
+                Point p = new Point(x, i, sym);
+                pList.Add(p);
+            }
+
+        }
+
+
+    }
+
+}
